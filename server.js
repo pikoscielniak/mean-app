884f1c01cfdf11ae116ce5806c87b3ad0a733196
piotrekbass <piotrek.koscielniak@gmail.com>
2014-02-15 21:36:44 +0100

    loggin with password

--- server.js
+++ server.js
@@ -1,63 +1,49 @@
 var express = require('express'),
-    stylus = require('stylus'),
-    mongoose = require('mongoose');
+    passport = require('passport'),
+    mongoose = require('mongoose'),
+    LocalStrategy = require('passport-local').Strategy;
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+var config = require('./server/config/config')[env];
 
-function compile(str, path) {
-    return stylus(str).set('filename', path);
-}
+require('./server/config/express')(app, config);
+require('./server/config/mongoose')(config);
 
-app.configure(function () {
-    app.set('views', __dirname + '/server/views');
-    app.set('view engine', 'jade');
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(stylus.middleware(
-        {
-            src: __dirname + '/public',
-            compile: compile
+var User = mongoose.model('User');
+passport.use(new LocalStrategy(
+    function (username, password, doen) {
+        User.findOne({username: username}).exec(function (err, user) {
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    if (user) {
+        done(null, user._id);
+    }
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findOne({_id: id}).exec(function (err, user) {
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
         }
-    ));
-    app.use(express.static(__dirname + '/public'));
-});
-
-app.get('/partials/:partialPath', function (req, res) {
-    res.render('partials/' + req.params.partialPath);
-});
-
-if (env === 'development') {
-    mongoose.connect('mongodb://localhost/multivision');
-} else {
-    mongoose.connect('mongodb://multivision:multivision@ds033079.mongolab.com:33079/multivision');
-}
-
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error...'));
-db.once('open', function callback() {
-    console.log('multivision db opened');
-});
-
-var messageSchema = mongoose.Schema({
-    message: String
-});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function (err, messageDoc) {
-    mongoMessage = messageDoc.message;
-});
-
-app.get('*', function (req, res) {
-    res.render('index', {
-        mongoMessage: mongoMessage
     });
 });
 
-var port = process.env.PORT || 3030;
-app.listen(port);
-console.log("Listening on port " + port + '...');
+require('./server/config/routes')(app);
+
+
+app.listen(config.port);
+console.log("Listening on port " + config.port + '...');
 
 
