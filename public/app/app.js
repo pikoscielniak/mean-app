c3629bef3c2f73eaeacc1f8e86452941d8590b6a
piotrekbass <piotrek.koscielniak@gmail.com>
2014-02-21 21:26:17 +0100

    courses

--- public/app/app.js
+++ public/app/app.js
@@ -6,7 +6,10 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
             auth: function (mvAuth) {
                 return mvAuth.authorizeCurrentUserForRoute('admin');
             }
-        }
+        },
+        user: {auth: function (mvAuth) {
+            return mvAuth.authorizeAuthenticatedUserForRoute()
+        }}
     };
 
 
@@ -20,6 +23,9 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
             resolve: routeRoleChecks.admin
         })
         .when('/signup', { templateUrl: '/partials/account/signup', controller: 'mvSignupCtrl'})
+        .when('/profile', { templateUrl: '/partials/account/profile',
+            controller: 'mvProfileCtrl', resolve: routeRoleChecks.user
+        })
 
 });
 
--- server/config/routes.js
+++ server/config/routes.js
@@ -4,8 +4,9 @@ var auth = require('./auth'),
 
 module.exports = function (app) {
 
-    app.get('/api/users', auth.requiresRole('admin'),users.getUsers);
-    app.post('/api/users',users.createUser);
+    app.get('/api/users', auth.requiresRole('admin'), users.getUsers);
+    app.post('/api/users', users.createUser);
+    app.put('/api/users', users.updateUser);
 
     app.get('/partials/*', function (req, res) {
         res.render('../../public/app/' + req.params);
@@ -16,6 +17,10 @@ module.exports = function (app) {
     app.post('/logout', function (req, res) {
         req.logout();
         res.end();
+    });
+
+    app.all('/api/*', function (req, res) {
+        res.send(404);
     });
 
     app.get('*', function (req, res) {
