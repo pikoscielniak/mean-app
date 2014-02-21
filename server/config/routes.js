35be82f8708c1647ad3991b02a6f078b6439f1e2
piotrekbass <piotrek.koscielniak@gmail.com>
2014-02-21 22:12:10 +0100

    courses list and detail with cache

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,17 @@
 var auth = require('./auth'),
     mongoose = require('mongoose'),
-    users = require('../controllers/users');
+    users = require('../controllers/users'),
+    courses = require('../controllers/courses');
 
 module.exports = function (app) {
 
     app.get('/api/users', auth.requiresRole('admin'), users.getUsers);
     app.post('/api/users', users.createUser);
     app.put('/api/users', users.updateUser);
+
+    app.get('/api/courses', courses.getCourses);
+    app.get('/api/courses/:id', courses.getCourseById);
+
 
     app.get('/partials/*', function (req, res) {
         res.render('../../public/app/' + req.params);
